44fc02ccf86c9ad2dcc278ca9174fd7287c05fa2
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-12-07 22:44:58 +0700

    Update

---
+++ Assets/Scripts/Coin/RespawnCoints.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RespawnCoints : MonoBehaviour
+{
+    [SerializeField] private Transform _pointsSpawn;
+    [SerializeField] private GameObject _coin;    
+
+    private void Awake()
+    {
+        for (int i = 0; i < _pointsSpawn.childCount; i++)
+        {
+            Instantiate(_coin, _pointsSpawn.GetChild(i));
+        }
+    }    
+}
---
+++ Assets/Scripts/Enemy/MoveEnemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    [SerializeField] private Transform _path;
+    [SerializeField] private float _speed;
+
+    private Transform[] _points;
+    private int _currentPoint;
+
+    void Start()
+    {
+        _points = new Transform[_path.childCount];
+
+        for (int i = 0; i < _path.childCount; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+    }
+    void Update()
+    {
+        Transform target = _points[_currentPoint];
+
+        if (_currentPoint == 0)
+        {
+            ChangeRotation(0, 180);
+        }
+        else
+        {
+            ChangeRotation();
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+
+        if (transform.position == target.position)
+        {
+            _currentPoint++;
+
+            if (_currentPoint >= _points.Length)
+            {
+                _currentPoint = 0;
+            }
+        }        
+    }
+    private void ChangeRotation(float x = 0, float y = 0)
+    {
+        transform.rotation = Quaternion.Euler(x, y, 0);
+    }
+}
